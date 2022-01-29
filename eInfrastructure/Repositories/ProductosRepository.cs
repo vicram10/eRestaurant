ac43ef80b5d0469e7940da268defd828dcc40645
vicram10 <vicram_42@hotmail.com>
2022-01-29 16:47:22 -0300

    creacion producto repositorio

---
+++ eInfrastructure/Repositories/ProductosRepository.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eInfrastructure.Models
+
+namespace eInfrastructure.Repositories
+{
+    public class ProductosRepository
+    {
+        public ProductosRepository(ConfiguracionesModel configuraciones, ApplicationDbContext dbContext, IHttpContextAccessor httpContextAccessor )
+        {
+            
+        }
+    }
+}
