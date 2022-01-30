282acfa3f438aedf0071f44c38fbb7d5a3358159
vicram10 <vicram_42@hotmail.com>
2022-01-30 13:45:23 -0300

    ahora terminamos el carrito, comenzamos conexion con AdamsPay

--- eInfrastructure/Languages/CarritoES.cs
+++ eInfrastructure/Languages/CarritoES.cs
@@ -6,6 +6,16 @@ namespace eInfrastructure.Languages
 {
     public class CarritoES
     {
+        ///labels
+        ///
+        public string CarritoTitle { get { return "Ver Carrito"; } }
+
+        public string lbTotalCarrito { get { return "Total a Pagar"; } }
+
+        public string lbFinalizarCompra { get { return "Preparar Pago"; } }
+
+        public string lbCheckOut { get { return "Ir a Ventana de Cobro"; } }
+
         ///mensajes
         ///
         public string msgNoSeleccionProducto { get { return "Debe especificar un producto para poder!!!"; } }
---
+++ eWebApi/Controllers/CarritoController.cs
@@ -0,0 +1,56 @@
+﻿using eInfrastructure.Entities;
+using eInfrastructure.Models;
+using eInfrastructure.Models.Carrito;
+using eService.Interfaces;
+using eWebApi.Filters;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eWebApi.Controllers
+{
+    [AuthorizationFilter]
+    public class CarritoController : Controller
+    {
+        private readonly ICarrito apiCarrito;
+
+        private readonly DatosUsuarioModel datosUsuario;
+
+        /// <summary>
+        /// constructor principal
+        /// </summary>
+        /// <param name="httpContextAccessor"></param>
+        /// <param name="apiCarrito"></param>
+        public CarritoController(IHttpContextAccessor httpContextAccessor, ICarrito apiCarrito)
+        {
+            this.apiCarrito = apiCarrito;
+
+            datosUsuario = new DatosUsuarioModel();
+
+            ISession session = httpContextAccessor.HttpContext.Session;
+
+            try
+            {
+                datosUsuario = JsonConvert.DeserializeObject<DatosUsuarioModel>(session.GetString("datosUsuario"));
+            }
+            catch { }
+        }
+
+        /// <summary>
+        /// para poder visualizar la lista de productos que existen en mi carrito de compra
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            List<Carrito> listadoCarrito = apiCarrito.Listar(new ParamFiltroBusquedaCarritoModel { IdUsuario = datosUsuario.IdUsuario, EstadoCarrito = EstadoCarritoModel.Pendiente });
+
+            ViewBag.ListadoCarrito = listadoCarrito;
+
+            return View();
+        }
+    }
+}
